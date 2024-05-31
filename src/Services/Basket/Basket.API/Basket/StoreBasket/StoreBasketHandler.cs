3b57a193bb0d33afd667248804acd1e7cbbf6f9e
Nazar Nyrka <nazarnyrka00@gmail.com>
2024-05-31 15:04:59 +0300

    Develop StoreBasket Feature Handler

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,24 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
-    
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        var cart = command.Cart;
+        
+        //Implement logic to store the cart
+        throw new NotImplementedException();
+    }
 }
