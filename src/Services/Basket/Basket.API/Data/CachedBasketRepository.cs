8aaee29f43834c329a7aaf4e3bca06c830a940e9
Nazar Nyrka <nazarnyrka00@gmail.com>
2024-06-02 20:56:34 +0300

    Fixed gitigore issue

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository) 
+    : IBasketRepository
+{
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return repository.GetBasket(userName, cancellationToken);
+    }
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        return repository.StoreBasket(cart, cancellationToken);
+    }
+
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return repository.DeleteBasket(userName, cancellationToken);
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,4 @@
+using BuildingBlocks.Exceptions.Handler;
 using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,10 +19,12 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
